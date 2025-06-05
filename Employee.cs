417dd2751591f8de1040651c0325ffab4df739e5
ufukkskc <150153686+ufukkskc@users.noreply.github.com>
2025-06-05 08:02:45 -0400

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Employee : IQuitable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public void Quit()
+    {
+        Console.WriteLine($"{FirstName} {LastName} has quit the company.");
+    }
+}
---
+++ IQuitable.cs
@@ -0,0 +1,4 @@
+﻿public interface IQuitable
+{
+    void Quit();
+}
