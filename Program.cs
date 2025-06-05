b96fb47a4cd3a59e8a6deaa7c020ca3a2623486b
ufukkskc <150153686+ufukkskc@users.noreply.github.com>
2025-06-05 08:04:04 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Polymorphism kullanarak bir Employee nesnesi IQuitable tipinde oluşturuluyor
+        IQuitable quitter = new Employee { FirstName = "Ufuk", LastName = "Kasikci" };
+
+        // Quit() metodu çağrılıyor
+        quitter.Quit();
+    }
+}
